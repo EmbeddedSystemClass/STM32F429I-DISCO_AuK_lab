a6c2e8e1aa2e4cea9944e5bd3e9fddce2b3eb66c
Szymon Bortel <szymonbortel8@gmail.com>
2018-06-11 21:45:49 +0200

    ddd

--- trace.h
+++ trace.h
@@ -3,10 +3,6 @@
 #define _TRACE_H_
 
 #include <stdint.h>
-<<<<<<< HEAD
-=======
-#include "task.h"
->>>>>>> 827e14f1f944406cef1cdb998ab0b297511468cb
 
 /** 
  * Initializes pins that will be used as trace signals.
